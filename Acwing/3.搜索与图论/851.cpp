276f3669e37cf97271bc18cf0bf2ac8a2b517223
misaka <1784321894@qq.com>
2022-06-10 12:20:52 +0800

    spfa

---
+++ Acwing/3.搜索与图论/851.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+const int N = 1e5 + 10;
+
+int n, m;
+int h[N], w[N], e[N], ne[N], idx;
+int dist[N];
+bool st[N];
+
+void add(int a, int b, int c) {
+    e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx ++ ;
+}
+
+int spfa() {
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+
+    queue<int> q;
+    q.push(1);
+    st[1] = true;
+
+    while(q.size()) {
+        int t = q.front();
+        q.pop();
+
+        st[t] = false;
+
+        for(int i = h[t]; ~i; i = ne[i]) {
+            int j = e[i];
+            if(dist[j] > dist[t] + w[i]) {
+                dist[j] = dist[t] + w[i];
+                if(!st[j]) {
+                    q.push(j);
+                    st[j] = true;
+                }
+            }
+        }
+    }
+
+    if(dist[n] == 0x3f3f3f3f) return -1;
+    return dist[n];
+}
+
+int main() {
+    cin >> n >> m;
+    memset(h, -1, sizeof h);
+
+    while(m -- ) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        add(a, b, c);
+    }
+
+    int t = spfa();
+
+    if(t == -1) cout << "impossible" << endl;
+    else cout << t << endl;
+
+    return 0;
+}
