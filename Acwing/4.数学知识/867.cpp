40cf176a810fb28cef02328139672f6e7bec39a9
misaka <1784321894@qq.com>
2022-06-11 17:33:36 +0800

    Knapsack

--- Acwing/4.数学知识/867.cpp
+++ Acwing/4.数学知识/867.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void divide(int n) {
-    for(int i = 2; i <= x / i; i ++ ) {
+    for(int i = 2; i <= n / i; i ++ ) {
         if(n % i == 0) {
             int s = 0;
             while(n % i == 0) {
