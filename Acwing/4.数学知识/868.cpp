ed9614551b3ff061098fbda446c2e24572eed958
misaka <1784321894@qq.com>
2022-06-11 15:39:20 +0800

    Prime

---
+++ Acwing/4.数学知识/868.cpp
@@ -0,0 +1,63 @@
+//---------------------------------------------------------------------------
+// 朴素筛法
+//---------------------------------------------------------------------------
+
+#include<iostream>
+using namespace std;
+
+const int N = 1e6 + 10;
+int primes[N], cnt;
+bool st[N];
+
+void get_primes(int n) {
+    for(int i = 2; i <= n; i ++ ) {
+        if(!st[i]) {
+            primes[cnt ++ ] = i;
+        }
+
+        for(int j = i + i; j <= n; j += i) {
+            st[j] = true;
+        }
+    }
+}
+
+
+//---------------------------------------------------------------------------
+// 埃氏筛法
+//---------------------------------------------------------------------------
+
+void get_Eratosthenes_primes(int n) {
+    for(int i = 2; i <= n; i ++ ) {
+        if(!st[i]) {
+            primes[cnt ++ ] = i;
+            for(int j = i + i; j <= n; j += i) {
+                st[j] = true;
+            }
+        }
+    }
+}
+
+//---------------------------------------------------------------------------
+// 埃氏筛法
+//---------------------------------------------------------------------------
+
+void get_liner_primes(int n) {
+    for(int i = 2; i <= n; i ++ ) {
+        if(!st[i]) {
+            primes[cnt ++ ] = i;
+        }
+        
+        for(int j = 0; primes[j] <= n / i; j ++ ) {
+            st[primes[j] * i] = true;
+            if(i % primes[j] == 0) break;
+        }
+    }
+}
+
+int main() {
+    int n; cin >> n;
+    get_primes(n);
+    cout << cnt << endl;
+
+    return 0;
+}
